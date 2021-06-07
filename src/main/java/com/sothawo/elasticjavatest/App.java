6047333233234318dcec22b20af8c459ff570147
Peter-Josef Meisch <pj.meisch@sothawo.com>
2021-06-07 06:36:20 +0200

    WIP

--- src/main/java/com/sothawo/elasticjavatest/App.java
+++ src/main/java/com/sothawo/elasticjavatest/App.java
@@ -6,9 +6,9 @@ package com.sothawo.elasticjavatest;
 import co.elastic.clients.base.RestClientTransport;
 import co.elastic.clients.base.Transport;
 import co.elastic.clients.elasticsearch.ElasticsearchClient;
+import co.elastic.clients.elasticsearch._global.SearchRequest;
 import co.elastic.clients.elasticsearch._global.SearchResponse;
 import co.elastic.clients.elasticsearch._global.search.Hit;
-import co.elastic.clients.json.jackson.JacksonJsonpMapper;
 import co.elastic.clients.json.jsonb.JsonbJsonpMapper;
 import org.apache.http.HttpHost;
 import org.elasticsearch.client.RestClient;
@@ -23,38 +23,40 @@ public class App {
     public static void main(String[] args) throws IOException {
 
         // Create the low-level client
-        RestClient restClient = RestClient.builder(new HttpHost("localhost", 9200))
+        var restClientBuilder = RestClient.builder(new HttpHost("localhost", 9200))
             .setHttpClientConfigCallback(builder -> {
                 builder.setProxy(new HttpHost("localhost", 8080));
                 return builder;
-            })
-            .build();
+            });
 
-        // Create the transport that provides JSON and http services to API clients
-        // Jackson crashes
-        // Transport transport = new RestClientTransport(restClient, new JacksonJsonpMapper());
-        // Yasson works
-        Transport transport = new RestClientTransport(restClient, new JsonbJsonpMapper());
+        // RestClient must be closed
+        try (RestClient restClient = restClientBuilder.build()) {
 
-        // And create our API client
-        ElasticsearchClient client = new ElasticsearchClient(transport);
+            // Create the transport that provides JSON and http services to API clients
+            Transport transport = new RestClientTransport(restClient, new JsonbJsonpMapper());
 
-        // Search all items in an index that contains documents of type AppData
-        SearchResponse<AppData> search = client.search(s -> s
-                .index("appdata-index"),
-            AppData.class
-        );
+            // And create our API client
+            ElasticsearchClient client = new ElasticsearchClient(transport);
 
-        if (search.hits().hits().isEmpty()) {
-            System.out.println("No match");
-        } else {
-            for (Hit<AppData> hit : search.hits().hits()) {
-                processAppData(hit._source());
+            // Search all items in an index that contains documents of type AppData
+            SearchResponse<StringObjectMap> search = client.search(s -> s
+                    .index("appdata-index"),
+                StringObjectMap.class
+            );
+
+            var builder = new SearchRequest.Builder();
+            builder.in
+            if (search.hits().hits().isEmpty()) {
+                System.out.println("No match");
+            } else {
+                for (Hit<StringObjectMap> hit : search.hits().hits()) {
+                    processAppData(hit._source());
+                }
             }
         }
     }
 
-    private static void processAppData(AppData appData) {
+    private static void processAppData(StringObjectMap appData) {
         if (appData != null) {
             System.out.println(appData.toString());
         }
